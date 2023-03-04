1bc425057bd6bbe48760f8a1ead1a5749f2a51e7
josepablopazcolindres <josepablopazcolindres@gmail.com>
2023-03-04 00:03:57 -0600

    se agrega el proyecto

---
+++ Condiciones/Condiciones/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sentancias
+{
+    class SentanciaIf
+    {
+        public static void Main(string[] args)
+        {
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+                Console.WriteLine("numeroEntero es 7. ");
+            else if (numeroEntero  < 0)
+                Console.WriteLine("numeroEntero es negativo");
+            else Console.WriteLine("numero entero no es ninguno de los casos programados");
+        }
+    }
+}
---
+++ Leer Teclado/Leer Teclado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeerTeclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos numeros");
+            Console.Write("ingresa numero 1: ");
+            string dato = Console.ReadLine();
+            int n1 = int.Parse(dato);
+
+            Console.WriteLine("ingrese numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("el resultado es: {0}", suma);
+        }
+
+    }
+}
+
+
+  
